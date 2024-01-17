72ab30b9136d884e06340d49d5f365e2a78d9ee3
gabrioviski <gabrielcarvalhos.snt@gmail.com>
2024-01-16 23:22:36 -0300

    calc.js aprimorado

--- js/calc.js
+++ js/calc.js
@@ -7,16 +7,50 @@ const operators = [...document.querySelectorAll('.buttons .symbols')].map(item =
 
 btns.forEach(btn => {
     btn.addEventListener('click', () => {
-        if (screen.textContent == '0') {
-            if (operators.includes(btn.textContent)) {
-                return
-            }
+        if (screen.textContent == '0' && !operators.includes(btn.textContent)) {
             screen.textContent = btn.textContent
             return
-        } 
-        screen.textContent += btn.dataset.operator || btn.textContent
+        }
+        
+        screen.textContent += btn.dataset.operator ? ` ${btn.dataset.operator} ` : btn.textContent
+        
+        console.log(screen.textContent.split(' '))
+
+        const len = screen.textContent.split(' ').length
+        if (len > 3) calc(btn.dataset.operator)
     })
 })
+
+result.addEventListener('click', () => {
+    calc()
+})
+
+const calc = (operatorAdded) => {
+    const [n1, operator, n2] = screen.textContent.split(' ')
+    // verifica se a expressao esta completa
+    if (n2) {
+        let res
+        switch (operator) {
+            case '+':
+                res = Number(n1) + Number(n2)
+                break;
+            case '-':
+                res = n1 - n2
+                break;
+            case '*':
+                res = n1 * n2
+                break;
+            case '/':
+                res = n1 / n2
+                break;
+            default:
+                res = 'erro'
+                break;
+        }
+        screen.textContent = operatorAdded ? `${res} ${operatorAdded} ` : res
+        console.log(n1, operator, n2, res, operatorAdded)
+    }
+}
 
 del.addEventListener('click', () => {
     screen.textContent = screen.textContent.slice(0, -1)
@@ -27,12 +61,4 @@ del.addEventListener('click', () => {
 
 clear.addEventListener('click', () => {
     screen.textContent = '0'
-})
-
-result.addEventListener('click', () => {
-    try {
-        screen.textContent = eval(screen.textContent)
-    } catch {
-        screen.textContent = '0'
-    }
 })
